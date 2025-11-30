855efec352d20000a3ecbf313c0c9fe1eca52452
x45iq <XRzDckOfjK@proton.me>
2025-11-30 14:19:23 +0200

    lab 5 commit

--- main.c
+++ main.c
@@ -1,84 +1,69 @@
-#include "ssd1306.h"
 #include <stdint.h>
-#include <stm32f10x.h>
+#include <stm32f10x.h>
+
+volatile uint8_t timer_flag = 0;
 
 void delay(uint32_t ticks) {
-  for (uint32_t i = 0; i < ticks; i++) {
+  for (int i = 0; i < ticks; i++) {
     __NOP();
   }
 }
 
-uint8_t chessboard_sizes[] = {4, 8, 16, 32};
-uint8_t current_size_index = 1;
-
 void handle_buttons(void) {
   if (!(GPIOC->IDR & GPIO_IDR_IDR14)) {
-    delay(50000);
+    delay(10000);
     if (!(GPIOC->IDR & GPIO_IDR_IDR14)) {
-      current_size_index++;
-      if (current_size_index >= sizeof(chessboard_sizes)) {
-        current_size_index = 0;
+      if (TIM2->PSC > 1) {
+        TIM2->PSC = TIM2->PSC >> 1;
       }
-
-      SSD1306_DrawChessboard(chessboard_sizes[current_size_index]);
-
-      GPIOC->ODR &= ~GPIO_ODR_ODR13;
-      delay(100000);
-      GPIOC->ODR |= GPIO_ODR_ODR13;
-
       while (!(GPIOC->IDR & GPIO_IDR_IDR14))
         ;
-      delay(50000);
     }
   }
 
   if (!(GPIOC->IDR & GPIO_IDR_IDR15)) {
-    delay(50000);
+    delay(10000);
     if (!(GPIOC->IDR & GPIO_IDR_IDR15)) {
-      if (current_size_index == 0) {
-        current_size_index = sizeof(chessboard_sizes) - 1;
-      } else {
-        current_size_index--;
+      if (TIM2->PSC < 0xFFFF) {
+        TIM2->PSC = TIM2->PSC << 1;
       }
-
-      SSD1306_DrawChessboard(chessboard_sizes[current_size_index]);
-
-      GPIOC->ODR &= ~GPIO_ODR_ODR13;
-      delay(100000);
-      GPIOC->ODR |= GPIO_ODR_ODR13;
-
       while (!(GPIOC->IDR & GPIO_IDR_IDR15))
         ;
-      delay(50000);
     }
+  }
+}
+
+void TIM2_IRQHandler(void) {
+  if (TIM2->SR & TIM_SR_UIF) {
+    TIM2->SR &= ~TIM_SR_UIF;
+    timer_flag = 1;
   }
 }
 
 int __attribute__((noreturn)) main(void) {
   RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
   GPIOC->CRH =
       (GPIOC->CRH & ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13)) | GPIO_CRH_MODE13_0;
-  GPIOC->ODR |= GPIO_ODR_ODR13;
 
   GPIOC->CRH = (GPIOC->CRH & ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14 |
                                GPIO_CRH_CNF15 | GPIO_CRH_MODE15)) |
                GPIO_CRH_CNF14_1 | GPIO_CRH_CNF15_1;
   GPIOC->ODR |= GPIO_ODR_ODR14 | GPIO_ODR_ODR15;
 
-  SSD1306_Init();
+  TIM2->PSC = 7999;
+  TIM2->ARR = 999;
+  TIM2->DIER |= TIM_DIER_UIE;
+  TIM2->CR1 |= TIM_CR1_CEN;
 
-  for (uint8_t i = 0; i < 3; i++) {
-    GPIOC->ODR ^= GPIO_ODR_ODR13;
-    delay(200000);
-  }
-  GPIOC->ODR |= GPIO_ODR_ODR13;
-
-  SSD1306_DrawChessboard(chessboard_sizes[current_size_index]);
-
+  NVIC_EnableIRQ(TIM2_IRQn);
   while (1) {
     handle_buttons();
 
-    delay(10000);
+    if (timer_flag) {
+      timer_flag = 0;
+      GPIOC->ODR ^= GPIO_ODR_ODR13;
+    }
   }
 }
